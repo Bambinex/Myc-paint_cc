0f62ae4234893745e0c13024e4d60de73a41e6b0
Bambinex <65712582+Bambinex@users.noreply.github.com>
2020-08-25 16:48:29 +0200

    Add files via upload

--- paint.py
+++ paint.py
@@ -2,8 +2,18 @@ import pygame
 
 pygame.init()
 
-screen = pygame.display.set_mode(size)
+screen = pygame.display.set_mode((800,800))
 
 while 1:
     for event in pygame.event.get():
         if event.type == pygame.QUIT: sys.exit()
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            pygame.mouse.get_pos()
+            pygame.mouse.get_rel()
+            screen.set_at(pygame.mouse.get_pos(), Color(255,255,255))
+    pygame.display.flip()
+        
+            
+        
+    
+    
